45116167d54bef18410d51ea3995a2ff4684fcd1
GoldenChrysus <GoldenChrysus@users.noreply.github.com>
2020-02-04 18:04:51 -0500

    Fix database and add environments

--- GeekText.Database/DbContextApplication.cs
+++ GeekText.Database/DbContextApplication.cs
@@ -1,5 +1,6 @@
 ﻿using GeekText.Domain.Models;
 using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure.Internal;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,10 +12,11 @@ namespace GeekText.Database
         public DbSet<Book> Books { get; set; }
         public DbSet<User> Users { get; set; }
 
+        public DbContextApplication(DbContextOptions<DbContextApplication> options) : base(options) {}
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseNpgsql("Host=ec2-34-197-171-33.compute-1.amazonaws.com;" +
-                "Database=dbqjkrlrbhs27k;Username=vlieqagwjdfdjh;" +
-                "Password=7415b25c9af8cbeaf88dc88e8af6e4bd80fe956ad9b6ce5047a9320dc7d5d917")
+            => optionsBuilder
+                .UseNpgsql(optionsBuilder.Options.FindExtension<NpgsqlOptionsExtension>().ConnectionString)
                 .UseSnakeCaseNamingConvention();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
