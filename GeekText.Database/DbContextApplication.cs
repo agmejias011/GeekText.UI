9607cedc1cb4cfb91396960ba606fdc1be7375d2
AGM <agmejias011@hotmail.com>
2020-02-08 14:54:39 -0500

    Models for Cart are created, successful connection to heroku database and table creatioin

--- GeekText.Database/DbContextApplication.cs
+++ GeekText.Database/DbContextApplication.cs
@@ -9,8 +9,12 @@ namespace GeekText.Database
 {
     public class DbContextApplication : DbContext
     {
+        //Add DBset for each class created under the Domain layer
         public DbSet<Book> Books { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Cart_Book> Cart_Books { get; set; }
 
         public DbContextApplication(DbContextOptions<DbContextApplication> options) : base(options) {}
 
@@ -19,9 +23,34 @@ namespace GeekText.Database
                 .UseNpgsql(optionsBuilder.Options.FindExtension<NpgsqlOptionsExtension>().ConnectionString)
                 .UseSnakeCaseNamingConvention();
 
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-            => modelBuilder.Entity<Book>().Property(b => b.id).UseIdentityAlwaysColumn()
+        {
+            //Code below is for the book and user to have a serial ID starting at 1000
+            //Do the same for any other class you need 
+            modelBuilder.Entity<Book>().Property(b => b.id).UseIdentityAlwaysColumn()
             .HasIdentityOptions(startValue: 1000);
+
+            modelBuilder.Entity<User>().Property(b => b.id).UseIdentityAlwaysColumn()
+            .HasIdentityOptions(startValue: 1000);
+
+            modelBuilder.Entity<Cart>().Property(b => b.id).UseIdentityAlwaysColumn()
+            .HasIdentityOptions(startValue: 1000);
+
+            modelBuilder.Entity<Order>().Property(b => b.id).UseIdentityAlwaysColumn()
+            .HasIdentityOptions(startValue: 1000);
+
+            modelBuilder.Entity<Cart_Book>(eb =>
+             {
+                 eb.HasNoKey();                
+             });
+
+
+            modelBuilder.HasDefaultSchema("public");
+            base.OnModelCreating(modelBuilder);
+
+        }
+
 
     }
 }
--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # GeekText
 
 ## Configuration
-
-1. Navigate to `/GeekText.UI`
-2. Copy `appsettings.Development.json.sample` to `appsettings.Development.json` and `appsettings.json`
-3. Change the `PgDatabase` connection string in the development file to your local PostgreSQL connection info.
-4. Change the `PgDatabase` connection string in the non-development file to the team's Heroku PostgreSQL connection info.
+'appsettings.json' contains the connection string for the team's Heroky PostgresSQL connection info.
+Everyone will be added to the database as collaborators with the email address
