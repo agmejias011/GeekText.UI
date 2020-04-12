96ca98f09a765c43d56081b6cc61cd17bcb4923b
NathanaelGar <35610908+NathanaelGar@users.noreply.github.com>
2020-04-12 09:10:08 -0400

    search bar conlfict

--- GeekText.UI/ClientApp/src/components/SearchBar.js
+++ GeekText.UI/ClientApp/src/components/SearchBar.js
@@ -1,86 +1,119 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import { FaShoppingCart, FaHeart, FaSearch } from "react-icons/fa";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import CartBar from "./ShoppingCart/CartBar";
-import MenuBookIcon from '@material-ui/icons/MenuBook';
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import Badge from "@material-ui/core/Badge";
+import { withStyles } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import CartList from "./ShoppingCart/cartList";
+import { connect } from 'react-redux'
 
-export default class SearchBar extends React.Component {
-  state = {
-    cartItemsTotal: 0,
-  };
-  componentWillMount() {
-    const cartItemsTotal = JSON.parse(localStorage.getItem("cartItemsTotal"));
-    if (cartItemsTotal) {
-      this.setState({ cartItemsTotal });
-    }
-  }
+export class SearchBar extends React.Component {
 
-  shouldComponentUpdate() {
-    const cartItemsTotal = JSON.parse(localStorage.getItem("cartItemsTotal"));
-    if (cartItemsTotal) {
-      this.setState({ cartItemsTotal });
-    }
-  }
+    renderSearchBarWithCartAmount() {
+        const { authenticated } = this.props;
 
-  renderSearchBarWithCartAmount() {
-    let account_buttons = this.props.user ? (
-      <Grid item>
-        <FavoriteIcon htmlColor="#ec407a" fontSize="large" />
-      </Grid>
-    ) : null;
-    let account_path = this.props.user ? "account" : "login";
-
-    return (
-      <>
-        <Grid
-          container
-          direction="row"
-          justify="center"
-          alignItems="center"
-          spacing={1}
-        >
-          
-          <Grid item md={2}>
-            <a href={"/"}>
-              <h2>GeekText</h2>
-            </a>
-          </Grid>        
-          <Grid item md={8}>
-            {/* TEMP SEARCH BAR  */}
-            <div className="searchBar">
-              <input
-                type="text"
-                className="search"
-                placeholder="Search for a book"
-              />
-              <button className="searchButton">
-                <FaSearch />
-              </button>
-            </div>
+        console.log(this.props.authenticated);
+        let account_buttons = this.props.authenticated ? (
+            <Grid item>
+                <FavoriteIcon htmlColor="#ec407a" fontSize="large" />
             </Grid>
-                <Grid item>
-                    <a href={"/books" }>
-                        <MenuBookIcon color="primary" fontSize="large" />
-                    </a>
-                </Grid>               
-          <Grid item>
-            <a href={"/" + account_path}>
-              <AccountCircleIcon color="primary" fontSize="large" />
-            </a>
-          </Grid>
-          <Grid item>
-            <CartBar></CartBar>
-          </Grid>
-          {account_buttons}
-        </Grid>
-        
-      </>
-    );
-  }
+        ) : null;
 
-  render() {
-    return this.renderSearchBarWithCartAmount();
-  }
+        let account_path = this.props.authenticated ? "profile" : "login";
+
+        if (this.props.itemsCartTotal === 0) {
+            return (
+                <>
+                    <Grid
+                        container
+                        direction="row"
+                        justify="center"
+                        alignItems="center"
+                        spacing={1}
+                    >
+                        <Grid item md={2}>
+                            <h2>GeekText</h2>
+                        </Grid>
+                        <Grid item md={8}>
+                            {/* TEMP SEARCH BAR  */}
+                            <div className="searchBar">
+                                <input
+                                    type="text"
+                                    className="search"
+                                    placeholder="Search for a book"
+                                />
+                                <button className="searchButton">
+                                    <FaSearch />
+                                </button>
+                            </div>
+                        </Grid>
+                        <Grid item>
+                            <a href={"/" + account_path}>
+                                <AccountCircleIcon color="primary" fontSize="large" />
+                            </a>
+                        </Grid>
+                        <Grid item>
+                            <CartBar />
+                        </Grid>
+                        {account_buttons}
+                    </Grid>
+                </>
+            );
+        }
+
+        return (
+            <>
+                <Grid
+                    container
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                    spacing={1}
+                >
+                    <Grid item md={2}>
+                        <h2>GeekText</h2>
+                    </Grid>
+                    <Grid item md={8}>
+                        {/* TEMP SEARCH BAR  */}
+                        <div className="searchBar">
+                            <input
+                                type="text"
+                                className="search"
+                                placeholder="Search for a book"
+                            />
+                            <button className="searchButton">
+                                <FaSearch />
+                            </button>
+                        </div>
+                    </Grid>
+                    <Grid item>
+                        <a href={"/" + account_path}>
+                            <AccountCircleIcon color="primary" fontSize="large" />
+                        </a>
+                    </Grid>
+                    <Grid item>
+                        <CartBar itemsTotal={this.props.itemsCartTotal}></CartBar>
+                    </Grid>
+                    {account_buttons}
+                </Grid>
+            </>
+        );
+    }
+
+    render() {
+        return this.renderSearchBarWithCartAmount();
+    }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        authenticated: state.authenticated
+    };
+}
+
+export default connect(mapStateToProps)(SearchBar);
